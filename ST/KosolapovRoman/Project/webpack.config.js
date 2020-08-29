3d93f43208ed98ddddceafd54ff1f974c2c356b4
RomanKosolapov <kosolapov-roman@list.ru>
2020-08-29 23:33:06 +0300

    Rebuild

--- ST/KosolapovRoman/Project/webpack.config.js
+++ ST/KosolapovRoman/Project/webpack.config.js
@@ -3,31 +3,33 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  output: {
-    filename: 'js/bundle.js'
-  },
-  module:{
-    rules:[
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      }
+    output: {
+        filename: 'js/bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|svg|gif)$/i,
+                use: { loader: 'file-loader' }
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+            chunkFilename: '[id].css'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: './src/assets/imgs', to: 'img' }
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
     ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].css',
-      chunkFilename: '[id].css'
-    }),
-    new CopyWebpackPlugin({
-      patterns:[
-        {from: 'src/assets/imgs', to: 'img'}
-      ]
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
-    })
-
-
-  ]
 }
