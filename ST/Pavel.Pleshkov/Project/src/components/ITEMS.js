85ee403f6c1218eff00b843a7621f5fe2bf70ea5
Pavel Pleshkov <pleshkov.p@gmail.com>
2020-08-29 13:58:22 +0300

    refactor: add super classes

---
+++ ST/Pavel.Pleshkov/Project/src/components/ITEMS.js
@@ -0,0 +1,33 @@
+import Item from './Item.js';
+
+export class CatalogItem extends Item {}
+
+export class BasketItem extends Item {
+    constructor(item) {
+        super(item);
+    }
+    render() {
+        return `
+        <div class="d-flex headerCartWrapIn mb-1 p-2">
+                <img src="${this.item.productImg}" alt="" width="85" height="100>
+                <div>
+                    <div>${this.item.productName}</div>
+                    <span>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </span>
+                    <div class="headerCartWrapPrice">${this.item.amount} 
+                        <span>x</span> $${this.item.productPrice}
+                    </div>
+            <button 
+                class="fas fa-times-circle" 
+                data-id="${this.item.productId}"
+                name="remove"
+            ></button>
+        </div>
+        `
+    }
+}
--- ST/Pavel.Pleshkov/Project/src/components/index.js
+++ ST/Pavel.Pleshkov/Project/src/components/index.js
@@ -1,12 +1,6 @@
-import Basket from './basket';
-import Catalog from './catalog';
+import { Basket, Catalog } from './LISTS';
 
 export default () => {
-  let catalogUrl = 'https://raw.githubusercontent.com/kellolo/static/master/JSON/catalog.json';
-  let basketgUrl = 'https://raw.githubusercontent.com/kellolo/static/master/JSON/basket.json';
-
-  let basket = new Basket('#basket', basketgUrl);
-  let catalog = new Catalog('#catalog', catalogUrl, basket);
-  // basket.init();
-  // catalog.init(basket); 
+    let basket = new Basket();
+    let catalog = new Catalog(basket);
 }
