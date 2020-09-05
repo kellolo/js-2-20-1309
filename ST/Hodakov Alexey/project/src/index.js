d528c05adc894eac66e59246e893f76d693beb50
AHodakov <hodakov.iphone@gmail.com>
2020-09-05 22:26:22 +0300

    add reduce

--- ST/Hodakov Alexey/project/src/index.js
+++ ST/Hodakov Alexey/project/src/index.js
@@ -2,5 +2,5 @@ import bootstrap from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./layout/styles/styles.css";
 
-import app from "./components";
+import app from "./components/index.js";
 app();
