15e2505953c2fb9ca37f68ab5ecaf8eab02349be
AHodakov <hodakov.iphone@gmail.com>
2020-09-06 00:27:42 +0300

    preparing for creating components

--- ST/Hodakov Alexey/project/src/index.js
+++ ST/Hodakov Alexey/project/src/index.js
@@ -1,6 +1,10 @@
 import bootstrap from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./layout/styles/styles.css";
+import Vue from "vue";
 
-import app from "./components/index.js";
-app();
+import app from "./views/main.vue";
+// app();
+new Vue({
+    render: h => h(app)
+}).$mount("#app");
