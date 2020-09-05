35a117232611c04bc632e96d0838c683b8ccf3bc
Sergey <sa.bugrov@mail.ru>
2020-09-05 21:37:12 +0500

    Lesson 5 Done. Vue in html.

--- ST/bugrov-sa/project/src/index.js
+++ ST/bugrov-sa/project/src/index.js
@@ -1,7 +1,14 @@
 import bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './layout/styles/styles.css'
+import Vue from 'vue';
 
 import app from './components';
 
 app();
+
+//import app from './views/mainpage.vue';
+
+// new Vue({
+//     render: h => h(app)
+// }).$mount('#app');
