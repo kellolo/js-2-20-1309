b8d73af02869b2423c2e5448cca25b50834cbb8a
AHodakov <hodakov.iphone@gmail.com>
2020-09-06 06:08:51 +0300

    create different components :)

--- ST/Hodakov Alexey/project/src/index.js
+++ ST/Hodakov Alexey/project/src/index.js
@@ -2,9 +2,8 @@ import bootstrap from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./layout/styles/styles.css";
 import Vue from "vue";
-
 import app from "./views/main.vue";
-// app();
+
 new Vue({
     render: h => h(app)
 }).$mount("#app");
